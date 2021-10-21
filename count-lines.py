8397a718510792503e4987b4f5339edd5f9731e3
siwarak-u <s.unsiwilai@tudelft.nl>
2021-10-21 14:45:42 +0200

    recover very first version of count-lines.py

--- count-lines.py
+++ count-lines.py
@@ -1,10 +1,3 @@
-"""
-This module counts the number of line in standard input
-Input: a string
-Output: total number of lines
-"""
-
-
 import sys
 
 count = 0
@@ -12,6 +5,7 @@ count = 0
 for line in sys.stdin:
 
 	count += 1
+
 
 print (count, "line in standard input")
 
